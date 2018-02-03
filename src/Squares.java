e09d7ca7bb8ce2b8b765b7ba4773deb4a6fd9d54
Kilmur <miiq@yandex.ru>
2018-02-03 20:49:42 +0300

    Add required parameter to the panel

--- src/Squares.java
+++ src/Squares.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Squares {
 
+    final static int SIZE = 10;
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
-        SquaresPanel squaresPanel = new SquaresPanel();
+        SquaresPanel squaresPanel = new SquaresPanel(SIZE);
         frame.add(squaresPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Квадратики");
