976caab15ccff044c1701508fd813f77550fcfab
isrusin <is_rusinov@mail.ru>
2018-02-05 01:25:27 +0300

    Add owner and repaint call to ClickListener

--- src/ClickListener.java
+++ src/ClickListener.java
@@ -4,21 +4,22 @@ import java.awt.event.ActionListener;
 public class ClickListener implements ActionListener{
 
     Field field;
-
-    public ClickListener(Field field) {
+    SquaresPanel owner;
+    public ClickListener(SquaresPanel owner, Field field) {
+        this.owner = owner;
         this.field = field;
     }
 
     @Override
-    public void actionPerformed(ActionEvent ae) {
-        CellButton parent = (CellButton) ae.getSource();
+    public void actionPerformed(ActionEvent event) {
+        CellButton parent = (CellButton) event.getSource();
         int x = parent.x;
         int y = parent.y;
-
         for(int i=x-1; i<=x+1; i++){
             for(int j=y-1; j<=y+1; j++){
                 field.nextState(i, j);
             }
         }
+        owner.updateButtonColors(x, y);
     }
 }
